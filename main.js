4265de425f8340705748ff2b5a9019a107ccc0d0
Kieran Busby <kebusby@hotmail.co.uk>
2023-09-22 14:56:41 +0100

    MVP has been made

--- main.js
+++ main.js
@@ -1,27 +1,45 @@
-let weatherData;
-
 async function returnWeather() {
     console.log("Fetch request is running...");
 
     try {
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,precipitation_hours&temperature_unit=fahrenheit&timezone=GMT");    
+        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,precipitation_hours&temperature_unit=fahrenheit&timezone=GMT");
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-
         const data = await response.json();
-        return data;
+        console.log(data)
+        return data
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
 }
 
-let usableData;
+async function changeText(){
+    const newText = await returnWeather();
 
-function thenHandler(d) {
-    usableData = d;
-    console.log(d);
+    let minTempOne = newText.daily.temperature_2m_min[0]
+
+    let maxTempOne = newText.daily.temperature_2m_max[0]
+    weatherDayOne.textContent = `Today's maxmimum temperature will be ${maxTempOne} degree's fehrenheit. The minimum will be ${minTempOne} fehrenheit`
 }
 
-returnWeather().then(thenHandler);
+// Create button event
+let button = document.getElementById("newWeatherBtn")
+
+
+// Target where the weather is going
+let weatherDayOne = document.getElementById("weatherDayOne")
+let weatherDayTwo = document.getElementById("weatherDayTwo")
+let weatherDayThree = document.getElementById("weatherDayThree")
+let weatherDayFour = document.getElementById("weatherDayFour")
+let weatherDayFive = document.getElementById("weatherDayFive")
+let weatherDaySix = document.getElementById("weatherDaySix")
+let weatherDaySeven = document.getElementById("weatherDaySeven")
+// console.log(weatherText)
+
+// Trigger event when button is clicked
+
+button.addEventListener("click", changeText)
+
+
 
