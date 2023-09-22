3c33aadf8509be90c2fb4bc8ff99fda1fd4bedb8
Kieran Busby <kebusby@hotmail.co.uk>
2023-09-22 16:05:37 +0100

    final ammendments

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// Create button event
+let button = document.getElementById("newWeatherBtn")
+// Trigger event when button is clicked
+button.addEventListener("click", changeText)
+
+// Async function which will complete API request.
+// Then parses the API response to return an OBJECT.
+
 async function returnWeather() {
     console.log("Fetch request is running...");
 
@@ -7,63 +15,55 @@ async function returnWeather() {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        // console.log(data)
         return data
     } catch (error) {
         console.error('Error fetching weather data:', error);
     }
 }
 
+// Function to pass in the API result.
+// Generate todays date.
 async function changeText(){
+    // Take returnWeather Function and store as variable
     const newText = await returnWeather();
 
     const todayDate = new Date();
     date.textContent = todayDate;
-
+    // If temperature is above 60 fahrenheit, change image to "sun"
     if (newText.daily.temperature_2m_max[0] > 60){
         let imageChange = document.getElementById("gif")
         imageChange.src = "./images/sun.gif"
     }
+    // else keep current image
 
-
+    // Parse API data and return new, required variables.
     let minTempOne = newText.daily.temperature_2m_min[0]
     let rainSum = newText.daily.rain_sum[0]
     let precipitationHours = newText.daily.precipitation_hours[0]
     let maxTempOne = newText.daily.temperature_2m_max[0]
 
-    // Target where the weather is going
+    // Target where the weather information is going.
     let weatherLineOne = document.getElementById("weatherLineOne")
     let weatherLineTwo = document.getElementById("weatherLineTwo")
     let weatherLineThree = document.getElementById("weatherLineThree")
     let weatherLineFour = document.getElementById("weatherLineFour")
 
-    weatherLineOne.textContent = `Today's maxmimum temperature will be ${maxTempOne} degree's fehrenheit. The minimum will be ${minTempOne} fehrenheit`;
-    weatherLineTwo.textContent = `The minimum will be ${minTempOne} fehrenheit`;
-    weatherLineThree.textContent = `The rain sum will be ${rainSum} mm`;
-    weatherLineFour.textContent = `The minimum precipitation hours will be ${precipitationHours} hours`;
+    // Variables to store weather info so we can console log easier too. 
+    let temptStatement = `Today's maxmimum temperature will be ${maxTempOne} degree's fehrenheit. The minimum will be ${minTempOne} fehrenheit`;
+    let temptStatement2 = `The minimum will be ${minTempOne} fehrenheit`;
+    let rainStatement = `The rain sum will be ${rainSum} mm`
+    let precipitationHrs = `The minimum precipitation hours will be ${precipitationHours} hours`
 
-    console.log(`Today's maxmimum temperature will be ${maxTempOne} degree's fehrenheit. The minimum will be ${minTempOne} fehrenheit`);
-    console.log(`The minimum will be ${minTempOne} fehrenheit`);
-    console.log(`The rain sum will be ${rainSum} mm`);
-    console.log(`The minimum precipitation hours will be ${precipitationHours} hours`);
+    // Ammend the text content in the HTML document with above results.
+    weatherLineOne.textContent = temptStatement
+    weatherLineTwo.textContent = temptStatement2;
+    weatherLineThree.textContent = rainStatement;
+    weatherLineFour.textContent = precipitationHrs;
+
+    console.log(temptStatement);
+    console.log(temptStatement2);
+    console.log(rainStatement);
+    console.log(precipitationHours);
 
 
 }
-
-// Create button event
-let button = document.getElementById("newWeatherBtn")
-
-
-// let weatherDayOne = document.getElementById("weatherDayOne")
-// let weatherDayTwo = document.getElementById("weatherDayTwo")
-// let weatherDayThree = document.getElementById("weatherDayThree")
-// let weatherDayFour = document.getElementById("weatherDayFour")
-// let weatherDayFive = document.getElementById("weatherDayFive")
-// let weatherDaySix = document.getElementById("weatherDaySix")
-// let weatherDaySeven = document.getElementById("weatherDaySeven")
-// console.log(weatherText)
-
-// Trigger event when button is clicked
-
-
-button.addEventListener("click", changeText)
