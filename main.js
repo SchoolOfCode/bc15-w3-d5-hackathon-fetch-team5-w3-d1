59a3185dd7471d675014faa297cba8a4f58a5649
Kieran Busby <kebusby@hotmail.co.uk>
2023-09-22 12:32:38 +0100

    Tinkering with the weather

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
-async function returnWeather () {
-    console.log("Fetch request is running...")
+// Document Object Targetting the p tag
+const weatherText = document.getElementById("weatherText")
 
+async function returnWeather () {
     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=51.5085&longitude=-0.1257&daily=weathercode,temperature_2m_max,temperature_2m_min,rain_sum,precipitation_hours&temperature_unit=fahrenheit&timezone=GMT")
     const data = await response.json()
-    return data
+    console.log(data)
 }
 
 returnWeather()
 
 // Document Object Targetting the button - eventListener
 const weatherButton = document.getElementById("newWeatherBtn")
+weatherButton.addEventListener("click", returnWeather)
 
 
-// Document Object Targetting the p tag
-const weatherText = document.getElementById("weatherText")
-
-let weatherData = returnWeather()
-
-console.log(weatherData)
